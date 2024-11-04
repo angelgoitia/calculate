922b952d616dba75a67877b830ef95238685f87b
angelgoitia <angelgoitia1995@gmail.com>
2024-11-04 10:19:02 -0400

    Initial commit

---
+++ app/Http/Controllers/CalculateController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CalculateController extends Controller
+{
+
+    public function getBodyMass(Request $request){
+        if(isset($request->weight) && isset($request->height)){
+            $weight = $request->weight;
+            $height = $request->height / 100;
+            $imc = ($weight / ($height * $height));
+            return response()->json(array('code' => 202, 'result' => number_format($imc, 2)));
+        }else{
+            return response()->json(array('code' => 401, 'message' => 'invalid parameters'));
+        }
+    }
+}
